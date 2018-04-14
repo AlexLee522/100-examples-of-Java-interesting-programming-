6e8b880e1208cb9e75459ad50edce1f6a65e484a
957 <32644012+AlexLee522@users.noreply.github.com>
2018-04-14 16:11:04 +0800

    Update ch1_2.java

--- ch1_2.java
+++ ch1_2.java
@@ -1,10 +1,8 @@
 //九九乘法表
 class ch1_2{
-	public static void main (String[] args) 
-	{
+	public static void main (String[] args) {
 		//控制行数
-		for (int i=1; i<10; i++) 
-		{
+		for (int i=1; i<10; i++) {
 			//控制每行表达式个数
 			for (int j=1; j<=i; j++) 
 			{
